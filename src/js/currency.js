860f9ac2be2f6f40f68037f282934ff71eb0b817
TrevorH08 <trevorh8700@gmail.com>
2022-09-09 16:45:13 -0700

    add API call in currency.js

---
+++ src/js/currency.js
@@ -0,0 +1,18 @@
+export default class CurrencyExchange {
+  static getExchange(currency) {
+    return promise = new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      const url = `GET https://v6.exchangerate-api.com/v6/${process.env.API_key}/latest/${currency}`;
+      request.addEventListener("loadend", function () {
+        const response = JSON.parse(this.responseText);
+        if (this.status === 200) {
+          resolve([response, currency]);
+        } else {
+          reject([this, response, currency]);
+        }  
+      });
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+}
