be9deaaf1caed7e5f6e33204e98c985910a8d486
TrevorH08 <trevorh8700@gmail.com>
2022-09-09 16:50:53 -0700

    add initial getExchange function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import CurrencyExchange from './js/currency.js';
+
+// Business Logic
+
+function getExchange(currency) {
+  let promise = CurrencyExchange.getExchange(currency);
+  promise.then(function(currencyDataArray) {
+    printElements(currencyDataArray);
+  }, function (errorArray) {
+    printError(errorArray);
+  });
+}
