a07dcd58cfecc21a3f71b40dcdac4ddee09ca60b
TrevorH08 <trevorh8700@gmail.com>
2022-09-21 01:15:18 -0700

    add api error message, bad currency, branch if bad currency

--- src/index.js
+++ src/index.js
@@ -11,25 +11,28 @@ function getExchange(currency) {
     printElements(currencyDataArray, currency);
   }, function (errorArray) {
     printError(errorArray);
-    console.log(errorArray);
   });
 }
 
 // UI Logic
 
 function printElements(data, currency) {
-  let rate = `${data[0].conversion_rates[currency]}`;
-  let userAmount = document.querySelector('#amountInput').value;
-  let convertedAmount = userAmount * rate;
-  document.querySelector('#rate').innerText = "Conversion Rate: $1 = " + rate + " " + currency
-  document.querySelector('#results').innerText = "Entered Amount: $" + userAmount + " = " + convertedAmount.toFixed(4) + " " + currency;
-  //LEFT OFF TRYING TO FIGURE OUT LINE ABOVE AND HOW TO PROPERLY CALL EXCHANGE RATE
-  console.log(currency);
+  document.querySelector('#results').innerText = "";
+  document.querySelector('#rate').innerText = "";
+  if (currency === "CAD" || currency === "JPY" || currency === "EUR" || currency === "SEK" || currency === "AUD") {
+    let rate = `${data[0].conversion_rates[currency]}`;
+    let userAmount = document.querySelector('#amountInput').value;
+    let convertedAmount = userAmount * rate;
+    document.querySelector('#rate').innerText = "Conversion Rate: $1 = " + rate + " " + currency;
+    document.querySelector('#results').innerText = "Entered Amount: $" + userAmount + " = " + convertedAmount.toFixed(4) + " " + currency;
+  } else {
+    document.querySelector('#results').innerText = "The Currency in question either can't be fetched, or doesn't exist. Please enter another."
+  }
 }
 
 function printError(error) {
   CurrencyExchange.response;
-  document.querySelector('#results').innerText = `There was an error accessing the exchange data for ${error[2]} `;
+  document.querySelector('#results').innerText = `There was an error accessing the exchange data for ${error[2]}: ${error[0].status} ${error[0].statusText}: ${error[0].message} `;
 }
 
 function handleFormSubmission(event) {
