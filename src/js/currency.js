a7963dd80d8c8b8a65815e7434c5144347d61d88
TrevorH08 <trevorh8700@gmail.com>
2022-09-20 22:32:12 -0700

    WIP fixing typos in API link and call

--- src/js/currency.js
+++ src/js/currency.js
@@ -2,7 +2,7 @@ export default class CurrencyExchange {
   static getExchange(currency) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      const url = `GET https://v6.exchangerate-api.com/v6/${process.env.API_key}/latest/USD`;
+      const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`;
       request.addEventListener("loadend", function () {
         const response = JSON.parse(this.responseText);
         if (this.status === 200) {
